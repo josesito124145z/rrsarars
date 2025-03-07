77d343d6e520eaa47969e6db9a303adb84da71da
josesito124145z <masterdmt12@hotmail.com>
2025-03-06 22:45:14 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="theme-color" content="#1ad9a0" />
+  <meta property="og:title" content="Club Penguin Leaks">
+  <meta property="og:image"
+    content="https://www.januarymedia.co.uk/files/technology/1679914637_the-rise-and-fall-of-club-penguin-january-media.webp">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="og:description" content="Club Penguin Leaks" />
+  <link rel="icon" href="https://cdn3.emoji.gg/emojis/46667-cutereindeer.png" type="image/webp">
+  <meta name="twitter:creator" content="">
+  <meta name="twitter:title" content="Club Penguin Leaks ">
+  <meta name="twitter:description" content="Club Penguin Leaks ">
+  <meta name="twitter:image"
+    content="https://www.januarymedia.co.uk/files/technology/1679914637_the-rise-and-fall-of-club-penguin-january-media.webp">
+  <meta name="twitter:image:alt" content="Club Penguin Leaks ">
+  <meta name="vk:image" content="">
+  <meta name="description" content="Club Penguin Leaks" />
+  <link rel="manifest" href="/manifest.json" />
+  <title>Club Penguin Leaks</title>
+  <link rel="stylesheet" href="/main.css">
+  <script src="/js/main.js"></script>
+</head>
+
+<body>
+  <nav>
+    <div class="navbar" style="background-color: rgb(32,33,36,255);">
+      <div class="container nav-container" style="background-color: rgb(32,33,36,255);">
+        <div class="logo">
+          <form action="index.html" method="GET" style="display: inline;">
+              <button type="submit" style="background: none; border: none; cursor: pointer;">
+                  <svg viewBox="0 0 24 24" class="svg-inline--fa fa-bell notifyIcon" xmlns="http://www.w3.org/2000/svg">
+                      <path fill="#FF0000"
+                          d="M2 6.5C2 4.01472 4.01472 2 6.5 2H12C14.2091 2 16 3.79086 16 6V7C16 7.55228 15.5523 8 15 8C14.4477 8 14 7.55228 14 7V6C14 4.89543 13.1046 4 12 4H6.5C5.11929 4 4 5.11929 4 6.5V17.5C4 18.8807 5.11929 20 6.5 20H12C13.1046 20 14 19.1046 14 18V17C14 16.4477 14.4477 16 15 16C15.5523 16 16 16.4477 16 17V18C16 20.2091 14.2091 22 12 22H6.5C4.01472 22 2 19.9853 2 17.5V6.5ZM18.2929 8.29289C18.6834 7.90237 19.3166 7.90237 19.7071 8.29289L22.7071 11.2929C23.0976 11.6834 23.0976 12.3166 22.7071 12.7071L19.7071 15.7071C19.3166 16.0976 18.6834 16.0976 18.2929 15.7071C17.9024 15.3166 17.9024 14.6834 18.2929 14.2929L19.5858 13L11 13C10.4477 13 10 12.5523 10 12C10 11.4477 10.4477 11 11 11L19.5858 11L18.2929 9.70711C17.9024 9.31658 17.9024 8.68342 18.2929 8.29289Z" />
+                    </svg>
+                </button>
+            </form>
+        </div>      
+        <div class="hamburger_menu" style="background-color: rgb(255, 255, 255);">
+          <input id="menu__toggle" type="checkbox" checked>
+          <label class="menu__btn" for="menu__toggle"><span></span></label>
+          <ul class="menu__box">
+            <div class="profileData">
+              <h3>Username: User9821</h3>
+              <h4>ID:  LXDD25F</h4>
+              <h4>
+                Tier: 
+                
+                    0
+                
+            </h4>      
+                  <h4>Invites: 0</h4>
+              <a href="getinvites.html">
+                <button>Invite Now</button>
+              </a>
+            </div>
+            <li><a class="menu__item" href="dashboard.html">Dashboard</a></li>
+            <li><a class="menu__item" href="preview.html">Free Preview</a></li>
+            <li><a class="menu__item" href="get.html">Get More Videos</a></li>
+            <li><a class="menu__item" href="invites.html">Invites Counting <span
+                  style="background-color: #FF0000; padding: 5px 10px; border-radius: 60px; color: rgb(255, 255, 255); margin-left: 0vh; display: inline-block;">
+                  0
+                </span></a></li>
+            <li><a class="menu__item" href="getinvites.html">Get Fast Invites</a></li>
+            <li><a class="menu__item" href="tier1.html">Free Tier 1</a></li>
+            <li><a class="menu__item" href="tier2.html">Free Tier 2</a></li>
+            <li><a class="menu__item" href="tier3.html">Free Tier 3</a></li>
+            <li><a class="menu__item" href="tier4.html">Free Tier 4</a></li>
+            <li><a class="menu__item" href="tier5.html">Free Tier 5</a></li>
+            <li><a class="menu__item" href="tier6.html">Free Tier 6</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+  
+  <div class="bottom-buttons">
+    <a href="https://t.me/FopplyDonky" target="_blank">
+      <button class="buyNow">Buy <br>Now!</button>
+    </a>
+  </div>
+
+  <div class="info-container">
+     <div class="profileData">
+              <h3>Username: User9821
+              </h3>
+              <h4>ID:  LXDD25F
+              </h4>
+              <h4>Tier: 
+                
+                    0
+                
+              </h4>
+              <h4>Invites: 0
+          
+            </div>
+            <div class="card">
+              <h3 class="center">Hello <span class="color">User9821</span></h3>
+              </a>
+              <h3>If you don't know how to view more content:</h3>
+              <a href="get.html">
+                <button>Get More Videos</button>
+              </a>
+              <h3>If you want to check your invite:</h3>
+              <a href="getinvites.html">
+                <button>Get Invites</button>
+              </a>
+            </div>
+            
+    <div class="card">
+      <h3>You have <span class="color">0</span> invites</h3>
+      <h3 style="text-align: center; margin-top: 20px;">Your Invite Link</h3>
+      <input type="text" readonly="" id="inviteLink" value="https://paradise667.xyz/register?invite=sDKWdV">
+      <button class="copyButton" onclick="copyInviteLink()">Copy link</button>
+      <h4>
+        Share your invite with your friends, or <span class="green">spam your invite link</span> on 
+        <span class="color">reddit</span>, <span class="color">twitter</span>, 
+        <span class="color">discord</span>, <span class="color">tumblr</span>, 
+        <span class="color">facebook</span>, or <span class="color">instagram</span>...
+      </h4>    </div>
+  </div>
+  
+  <script>
+    function copyInviteLink() {
+  const inviteLinkInput = document.getElementById("inviteLink");
+  inviteLinkInput.removeAttribute("readonly"); 
+  inviteLinkInput.select();
+  document.execCommand("copy"); 
+  inviteLinkInput.setAttribute("readonly", ""); 
+
+  alert("Link copied to clipboard!");
+      }
+    
+  </script>
+  
+</body>
+</html>
